4f4a18ae9d54efcc9444cedfbb54620a71783d1c
Sherzod Mamatmurodov <sherzodm214@gmail.com>
2025-06-21 00:33:20 +0500

    build: optimize production release

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,19 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		modules: {
+			generateScopedName:
+				process.env.NODE_ENV === 'production'
+					? '[hash:base64:8]' // Use only hashed class names in production
+					: '[local]__[hash:base64:5]', // Default format for development,
+		},
+		preprocessorOptions: {
+			scss: {
+				// additionalData: `@use "sass:math";`, // Include any global SCSS configurations here
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			'@/app': path.resolve(__dirname, 'src/app'),
@@ -24,6 +37,30 @@ export default defineConfig({
 	},
 	build: {
 		target: 'ES2022',
-		sourcemap: false, // disable source map on production
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				entryFileNames: 'root/[name].js',
+				chunkFileNames: chunk => {
+					const moduleId = chunk.facadeModuleId
+						? path.parse(chunk.facadeModuleId).name
+						: chunk.name;
+					if (moduleId.startsWith('_SVG')) {
+						return 'svgs/[name]-[hash].js';
+					}
+					return 'root/[name]-[hash].js';
+				},
+				assetFileNames: info => {
+					const fileName = info.names && info.names.length > 0 && info.names[0];
+					if (fileName && /\.(gif|jpe?g|png|svg|ico|webp)$/.test(fileName)) {
+						return 'assets/images/[name]-[hash][extname]';
+					}
+					if (fileName && /\.(css)$/.test(fileName)) {
+						return 'assets/css/[name]-[hash][extname]';
+					}
+					return 'assets/[name]-[hash][extname]';
+				},
+			},
+		},
 	},
 });
