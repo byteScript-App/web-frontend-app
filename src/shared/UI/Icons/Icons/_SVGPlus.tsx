6f8f402d9a40dfb5734a83fdb73f935bf49cce90
Sherzod Mamatmurodov <sherzodm214@gmail.com>
2025-06-20 23:15:09 +0500

    feat: Icons SVGs

--- src/shared/UI/Icons/Icons/_SVGPlus.tsx
+++ src/shared/UI/Icons/Icons/_SVGPlus.tsx
@@ -3,12 +3,14 @@ import { FC } from 'react';
 import { IconsProps } from '@/shared/UI';
 import { cls } from '@/shared/utils';
 
-const _SvgPlus: FC<IconsProps> = ({ width = 48, lineHeight = 48, className, color }) => {
+import styles from './Icons.module.scss';
+
+const _SvgPlus: FC<IconsProps> = ({ width = 48, lineHeight = 48, color, className }) => {
 	return (
 		<svg
 			width={width}
 			height={lineHeight}
-			className={cls('icon', color, className)}
+			className={cls(styles.icon, color && styles[color], className)}
 			viewBox='0 0 24 24'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
